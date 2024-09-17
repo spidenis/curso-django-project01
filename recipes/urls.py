527a3ee160e50c92827dc66a9f26a785cbc4412e
Valdenilson de Melo Nascimento <valdenilson.melo@ifac.edu.br>
2024-09-17 13:56:56 -0500

    Add page view of contact and home and also modifyed urls and views

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from recipes.views import my_first_response
+from recipes.views import my_first_response, my_first_contact
  
 urlpatterns = [
     path('', my_first_response),
+    path('contact/', my_first_contact),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -3,4 +3,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def my_first_response(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/home.html', context={
+        'name': 'Valdenilson'
+    })
+
+def my_first_contact(request):
+    return render(request, 'recipes/contact.html', context={
+        'name': '68981159337'
+    })
