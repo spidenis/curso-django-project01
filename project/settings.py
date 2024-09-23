102c276839237d49d12dda1f21905cfc740ab5eb
Valdenilson de Melo Nascimento <valdenilson.melo@ifac.edu.br>
2024-09-23 16:57:20 -0500

    Create file requirements and configure image opening

--- project/settings.py
+++ project/settings.py
@@ -123,6 +123,9 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / 'static'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
--- recipes/models.py
+++ recipes/models.py
@@ -1,6 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+
+    def __str__(self):
+        return self.name
 
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
@@ -16,3 +23,5 @@ class Recipe(models.Model):
     updated_at = models.DateField(auto_now=True)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+    categegory = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
