1f558c8f77e66ba923197ca1fde1fd93ed5a7a80
Valdenilson de Melo Nascimento <valdenilson.melo@ifac.edu.br>
2024-09-26 14:48:22 -0500

    Hidden the author if the username to be empty

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
 from .models import Recipe
@@ -15,8 +16,12 @@ def home(request):
 def category(request, category_id):
     recipes = Recipe.objects.filter(categegory__id=category_id, is_published=True).order_by('-id')
 
+    if not recipes:
+        raise Http404('Not found :(')
+
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+        'title': f'{recipes.first().categegory.name}  - Category |'
     })
 
 def recipes(request, id):
