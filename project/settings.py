47711f5275b929a9b4e1e65ac44aa35826cdd298
Valdenilson de Melo Nascimento <valdenilson.melo@ifac.edu.br>
2024-10-07 16:54:20 -0500

    Page static with css file available

--- project/settings.py
+++ project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--83sclvskydb#r(4%#al8504(l9lcmk%1k3(1t=m$08er1vm_9'
+SECRET_KEY = 'django-insecure--83sclvskydb#r(4%#al8504(l9lcmk%1k3(1t=m$08er1vm_9' #noqa: E501
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -111,9 +111,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
